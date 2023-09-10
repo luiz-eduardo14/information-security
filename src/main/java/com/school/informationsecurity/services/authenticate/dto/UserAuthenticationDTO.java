6aa46afc42d2d36453350116f2049300b53d07e8
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-09-10 00:41:52 -0300

    feat: create dto for storage info of user in authentication

---
+++ src/main/java/com/school/informationsecurity/services/authenticate/dto/UserAuthenticationDTO.java
@@ -0,0 +1,14 @@
+package com.school.informationsecurity.services.authenticate.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserAuthenticationDTO {
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
