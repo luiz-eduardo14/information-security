4ff86c5e6a92ce11ac88451ffa5ff427f20704ba
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-09-10 00:41:15 -0300

    feat: create fservice for signin user and signup user

---
+++ src/main/java/com/school/informationsecurity/services/authenticate/AuthenticateService.java
@@ -0,0 +1,41 @@
+package com.school.informationsecurity.services.authenticate;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.school.informationsecurity.entities.User;
+import com.school.informationsecurity.repository.UserRepository;
+import com.school.informationsecurity.security.JwtTokenUtil;
+import com.school.informationsecurity.services.authenticate.dto.JwtResponseDTO;
+import com.school.informationsecurity.services.authenticate.dto.UserAuthenticationDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticateService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtil jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public JwtResponseDTO signup(UserAuthenticationDTO dto) {
+        User user = User.builder().firstName(dto.getFirstName()).lastName(dto.getLastName())
+                .email(dto.getEmail()).password(passwordEncoder.encode(dto.getPassword()))
+                .build();
+        userRepository.save(user);
+        String jwt = jwtService.generateToken(user);
+        return JwtResponseDTO.builder().token(jwt).build();
+    }
+
+    public JwtResponseDTO signin(UserAuthenticationDTO dto) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword()));
+        User user = userRepository.findByEmail(dto.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password"));
+        String jwt = jwtService.generateToken(user);
+        return JwtResponseDTO.builder().token(jwt).build();
+    }
+}
