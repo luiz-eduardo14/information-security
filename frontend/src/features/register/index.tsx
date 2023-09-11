90c7da7f5a1912b7f17294d1dae76cdb483bcd25
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-09-11 01:05:12 -0300

    feat: implement isLoading in Register with react-query

--- frontend/src/features/register/index.tsx
+++ frontend/src/features/register/index.tsx
@@ -2,29 +2,49 @@ import { useForm } from "react-hook-form";
 import "../style/form/style.css";
 import { FormTextInput } from "../components/FormTextInput";
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { CircleLoader } from "react-spinners";
+import { fetchApi } from "../services/api";
 export function Register() {
 
     const { handleSubmit } = useForm();
 
-    return <section className="container forms">
-                <div className="form login">
-                    <div className="form-content">
-                        <header>Register</header>
-                        <form onSubmit={handleSubmit((data) => console.log(data))}>
-                            <FormTextInput type="email" placeholder="Email" name="email" />
-                            <FormTextInput type="password" placeholder="Password" name="password">
-                                <i className='bx bx-hide eye-icon'></i>
-                            </FormTextInput>
-                            <FormTextInput type="text" placeholder="First Name" name="firstName" />
-                            <FormTextInput type="text" placeholder="Last Name" name="lastName" />
-                            <div className="field button-field">
-                                <button>Signup</button>
-                            </div>
-                        </form>
-                        <div className="form-link">
-                            <span>Have an account? <Link to="/signin" className="link signup-link">Signin</Link></span>
+    const { isLoading, error, data } = useQuery({
+        queryFn: () => fetchApi({
+            url: 'api/auth/signup',
+            method: 'POST'
+        }),
+        queryKey: 'signup',
+    });
+
+    return (
+            <section className="container forms">
+                    <div className="form login">
+                        <div className="form-content">
+                                <header>Register</header>
+                                <form onSubmit={handleSubmit((data) => console.log(data))}>
+                                    <FormTextInput type="email" placeholder="Email" name="email" required />
+                                    <FormTextInput type="password" placeholder="Password" name="password" required>
+                                        <i className='bx bx-hide eye-icon'></i>
+                                    </FormTextInput>
+                                    <FormTextInput type="text" placeholder="First Name" name="firstName" required />
+                                    <FormTextInput type="text" placeholder="Last Name" name="lastName" required />
+                                    <div className="field button-field">
+                                        <button disabled={isLoading}>
+                                            {isLoading && <span><CircleLoader loading={!isLoading} size={15} /></span>}
+                                            {!isLoading && <span>Signup</span>}
+                                        </button>
+                                    </div>
+                                </form>
+                                <div className="form-link">
+                                    {!isLoading && 
+                                        <span>Have an account?
+                                            <Link to="/signin" className="link signup-link"> Signin</Link>
+                                        </span>
+                                    }
+                                </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+    )
 }
