7cae8147d55b1dae3e4e7b07da5759ac31ed78b6
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-09-11 01:04:53 -0300

    feat: change type for use type function Partial

--- frontend/src/features/services/api.ts
+++ frontend/src/features/services/api.ts
@@ -1,6 +1,6 @@
 const baseUrl = import.meta.env.VITE_API_URL;
 
-function fetchApi(fetchInfo: RequestInfo): Promise<Response> {
+export function fetchApi(fetchInfo: Partial<RequestInfo>): Promise<Response> {    
     return fetch(fetchInfo instanceof String ? baseUrl + fetchInfo : {
         ...(fetchInfo as Request),
         url: baseUrl + (fetchInfo as Request).url
