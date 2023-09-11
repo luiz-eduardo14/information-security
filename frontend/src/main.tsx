1ceb883e6752a334ff1ff40dac48e38f41b97d92
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-09-10 23:59:58 -0300

    feat: implement query provider of react query in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Router } from './router/index.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router />
+    <QueryClientProvider client={new QueryClient()}>
+      <Router />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
