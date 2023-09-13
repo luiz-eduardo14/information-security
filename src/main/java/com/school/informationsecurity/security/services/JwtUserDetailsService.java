05c8e68334090d3b4fcfee27d01fc4466608125d
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-09-13 00:08:05 -0300

    feat(backend): move of service directory for security directory

--- src/main/java/com/school/informationsecurity/security/services/JwtUserDetailsService.java
+++ src/main/java/com/school/informationsecurity/security/services/JwtUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.school.informationsecurity.services.authenticate;
+package com.school.informationsecurity.security.services;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
