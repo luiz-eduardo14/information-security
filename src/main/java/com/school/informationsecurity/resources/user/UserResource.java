7f477c6828f4a4dc897ca979ccf8726ca6bc8dc4
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-09-15 00:16:32 -0300

    feat(backend): create endpoint for get current info user

---
+++ src/main/java/com/school/informationsecurity/resources/user/UserResource.java
@@ -0,0 +1,24 @@
+package com.school.informationsecurity.resources.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.school.informationsecurity.common.SecurityUtils;
+import com.school.informationsecurity.services.user.UserService;
+import com.school.informationsecurity.services.user.dto.UserDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserResource {
+
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public UserDTO getUser() {
+        return userService.getUser(SecurityUtils.getCurrentUsername());
+    }
+}
---
+++ src/main/java/com/school/informationsecurity/services/user/UserService.java
@@ -0,0 +1,24 @@
+package com.school.informationsecurity.services.user;
+
+import org.springframework.stereotype.Service;
+
+import com.school.informationsecurity.repository.UserRepository;
+import com.school.informationsecurity.services.user.dto.UserDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserDTO getUser(String email) {
+        return userRepository.findByEmail(email).map(user -> {
+            return UserDTO.builder()
+                    .email(user.getEmail())
+                    .firstName(user.getFirstName())
+                    .lastName(user.getLastName())
+                    .build();
+        }).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
