3f8fdde05b8bcc67544854d2c1a94e56711b454c
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-09-30 17:50:00 -0300

    feat: create component for display cards with message in chat

---
+++ frontend/src/features/chat/components/CardMessage.tsx
@@ -0,0 +1,45 @@
+import { useMemo } from "react";
+import { useAuthentication } from "../../../hooks/useAuthentication";
+
+type CardMessageProps = {
+    type: 'FILE' | 'TEXT';
+    message: string;
+    sender: string;
+    date: Date;
+    username: string;
+};
+
+export function CardMessage({
+    message,
+    date,
+    sender,
+    username,
+}: CardMessageProps) {
+    const { user } = useAuthentication();
+    
+    const isMyMessage = useMemo(() => {
+        return user?.email === sender;
+    }, [user, sender]);
+
+    const formattedDate = useMemo(() => {
+        return date?.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+        });
+    }, [date]);
+
+    return (
+    <li className={isMyMessage ? 'me' : 'you'}>
+        <div className="entete">
+            <span className={`status ${isMyMessage ? 'green' : 'blue'}`}></span>
+            <h2>{username}</h2>
+            <h3>{formattedDate}</h3>
+        </div>
+        <div className="triangle"></div>
+        <div className="message">
+            {message}
+        </div>
+    </li>
+    );
+}
