b399f3e596792d8c53336208e45e5b8693e6fb96
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-03 01:36:24 -0300

    feat: persist message before return message in websocket

--- src/main/java/com/school/informationsecurity/resources/chat/ChatSocketResoure.java
+++ src/main/java/com/school/informationsecurity/resources/chat/ChatSocketResoure.java
@@ -1,33 +1,33 @@
 package com.school.informationsecurity.resources.chat;
 
-import java.security.Principal;
-import java.security.Security;
-import java.util.Optional;
-
-import com.school.informationsecurity.common.SecurityUtils;
+import com.school.informationsecurity.services.chat.ChatService;
 import com.school.informationsecurity.services.chat.dto.MessageRequestDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
-import lombok.RequiredArgsConstructor;
-
 @Controller
 @RequiredArgsConstructor
 public class ChatSocketResoure {
 
-    private final SimpMessagingTemplate messagingTemplate;
-    
-    @MessageMapping("/chat")
-    @SendToUser("/topic/chat")
-    public MessageRequestDTO chat(MessageRequestDTO message) {
-        return message;
-    }
+  private final SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/chat/{username}")
-    public void chat(MessageRequestDTO message, @DestinationVariable String username) {
-        this.messagingTemplate.convertAndSendToUser(username, "/topic/chat", message);
-    }
+  private final ChatService chatService;
+
+  @MessageMapping("/chat")
+  @SendToUser("/topic/chat")
+  public MessageRequestDTO chat(MessageRequestDTO message) throws Exception {
+    chatService.sendMessage(message.getSender(), message.getReceiver(), message.getMessage());
+    return message;
+  }
+
+  @MessageMapping("/chat/{username}")
+  public void chat(MessageRequestDTO message, @DestinationVariable String username)
+      throws Exception {
+    this.chatService.sendMessage(message.getSender(), username, message.getMessage());
+    this.messagingTemplate.convertAndSendToUser(username, "/topic/chat", message);
+  }
 }
