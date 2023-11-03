ca7623d316dfea26fc99a2404a30e41b1f0abbb0
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-03 01:35:17 -0300

    feat: add builder for ChatResponseDTO

--- src/main/java/com/school/informationsecurity/services/chat/dto/ChatResponseDTO.java
+++ src/main/java/com/school/informationsecurity/services/chat/dto/ChatResponseDTO.java
@@ -1,10 +1,12 @@
 package com.school.informationsecurity.services.chat.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class ChatResponseDTO {
     private List<MessageResponseDTO> messages;
     private String sender;
