a43a4255049948601ce03758e8d7bdb9cd0b4ca0
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-12 17:56:37 -0300

    feat: create modal for sign message

--- frontend/src/features/chat/index.tsx
+++ frontend/src/features/chat/index.tsx
@@ -10,6 +10,9 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { chatActions } from "../../store/reducers/chatReducer";
 import { useAuthentication } from "../../hooks/useAuthentication";
+import Modal from 'react-modal';
+import { useForm } from "react-hook-form";
+import { Toast } from "../../utils/Toast";
 
 export function Chat() {
 
@@ -25,10 +28,69 @@ export function Chat() {
 
 	const { user } = useAuthentication();
 
+	const [modalIsOpen, setIsOpen] = useState(false);
+
+	const form = useForm<{
+		signMessage: boolean;
+	}>();
+
+	const sendMessageCallback = (formValues: {
+		signMessage: boolean;
+	}) => {
+		if (socket?.connected && user) {
+			socket.publish({
+				destination: '/app/chat/' + receiver,
+				body: JSON.stringify({
+					message: textArea,
+					sender: user.email,
+					name: user.firstName,
+					signMessage: formValues.signMessage,
+				}),
+			});
+			dispatch(chatActions.addMessage({
+				message: textArea,
+				sender: user.email,
+				name: user.firstName,
+			}));
+			setIsOpen(false);
+			setTextArea(null);
+		}
+	}
+
     return (
 	<div style={{
 		paddingTop: "50px",
 	}}>
+		<Modal isOpen={modalIsOpen} style={{
+			content: {
+				width: '600px',
+				height: '200px',
+				left: 0,
+				right: 0,
+				margin: '0 auto'
+			},
+			overlay: {
+				backgroundColor: 'rgba(0,0,0,0.5)',
+			},
+		}}>
+				<div className="form login">
+					<form onSubmit={form.handleSubmit(sendMessageCallback)}>
+						<div style={{
+							display: "flex",
+							gap: "8px",
+						}}>
+							<input type="checkbox" {...form.register("signMessage")} />
+							<label>
+								Gostaria de assinar essa mensagem?
+							</label>
+						</div>
+						<div>
+							<button onClick={() => setIsOpen(false)}>Cancelar</button>
+							<button type="submit">Enviar</button>
+						</div>
+					</form>
+				</div>
+		</Modal>
 		<div id="container">
 			<aside>
 				<Search />
@@ -53,28 +115,19 @@ export function Chat() {
 					alignItems: "center",
 				}}>
 					<textarea 
-					onChange={(e) => setTextArea(e.target.value)}
-					style={{
-						width: "100%",
-					}} placeholder="Write your message"></textarea>
+						onChange={(e) => setTextArea(e.target.value)}
+						value={textArea as any}
+						style={{
+							width: "100%",
+						}} placeholder="Write your message">
+					</textarea>
 					<a 
 					onClick={() => {
-						if (socket?.connected) {
-							console.log('send message');
-							socket.publish({
-								destination: '/app/chat/' + receiver,
-								body: JSON.stringify({
-									message: textArea,
-									sender: user?.email,
-									name: user?.firstName,
-								}),
-							});
-							dispatch(chatActions.addMessage({
-								message: textArea,
-								sender: user?.email,
-								name: user?.firstName,
-							}));
+						if (!textArea && !textArea?.trim()) {
+							Toast.showErrorMessage('Message cannot be empty');
+							return;
 						}
+						setIsOpen(true);
 					}}
 					style={{
 						textAlign: "center",
